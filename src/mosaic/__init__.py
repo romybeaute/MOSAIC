be9ceb7685fc88a4c0f59f13e5cbd77489a87c1b
romybeaute <romybeaute.univ@gmail.com>
2026-01-28 18:20:11 -0300

    cleaned the flow

--- src/mosaic/__init__.py
+++ src/mosaic/__init__.py
@@ -1,1 +1,14 @@
-__all__ = ["path_utils"]
+# src/mosaic/__init__.py
+
+from .model import run_bertopic, setup_model
+
+from .path_utils import *
+
+
+from .preprocessing import preprocessing
+
+__all__ = [
+    "run_bertopic",
+    "setup_model",
+    "preprocessing"
+]
